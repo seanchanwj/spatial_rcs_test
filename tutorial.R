4b9f41a58307a8ed4af68a109fda650d4c592521
seanchanwj <65308164+seanchanwj@users.noreply.github.com>
2023-06-30 01:20:49 +0800

    New scripts update for MacOS after testing

--- tutorial.R
+++ tutorial.R
@@ -6,9 +6,11 @@ library(VGAM)
 library(tensorflow)
 library(reticulate)
 library(scales)
+library(tidyverse)
+library(lubridate)
 
 #set working directory to directory where the function and simulated data are stored
-setwd( "INSERTPATH" )
+setwd( "/Users/seanchanwj/Library/CloudStorage/OneDrive-Personal/MSc_PhD Public Health/Research/Malaria/Spatial_NetRateMalBhu|_test" )
 
 # source function to generate RC
 source("function_genRC.R")
@@ -20,13 +22,51 @@ reticulate::install_miniconda(path = "../AppData/Local/r-miniconda/", update = T
 
 Sys.setenv(RETICULATE_PYTHON="../AppData/Local/r-miniconda/envs/tensorflow")
 tensorflow::install_tensorflow(version = "1.14",
-                               extra_packages = c("tensorflow-probability==0.7" "scipy==1.5.0")  ,
+                               extra_packages = c("tensorflow-probability==0.7", "scipy==1.5.0")  ,
                                conda_python_version = "3.6",
                                envname              = "tensorflow")
 conda_list()
 use_condaenv("tensorflow", required = TRUE)
 tfp <- reticulate::import("tensorflow_probability",convert=FALSE)
+
+#For MacOS, try using the following installation: 
+
+  #Install Miniconda: https://docs.conda.io/en/main/miniconda.html
+  #Open Terminal
+  #Create Conda env: conda create --name rstudio-tf-2.8
+  #Activate env: conda activate rstudio-tf-2.8
+  #Downgrade Python: conda install python=3.8
+  #Install tensorflow-deps: conda install -c apple tensorflow-deps==2.8.0
+  #Install tensorflow-macos: python -m pip install tensorflow-macos==2.8.0
+  #Install tensorflow-metal: python -m pip install tensorflow-metal
+  #Install tensorflow: conda install -c anaconda tensorflow
+  
+  ##(Depending on versions)
+  
+
+  #Edit ~/.Renviron: echo "RETICULATE_PYTHON=~/miniforge3/envs/rstudio-tf-2.8/bin/python" >> ~/.Renviron
+  #Check installed packages: conda list | grep tensorflow
+
+  #Go to R-Studio and restart R-Session Session -> Restart R
+  #Init and test tensorflow with following R script:
+  #reticulate::use_python("~/miniconda3/envs/rstudio-tf-2.8/bin/python")
+  ##reticulate::use_condaenv("rstudio-tf-2.8", conda="~/miniconda3/bin/conda", required = TRUE)
+
+
+reticulate::use_python("~/miniconda3/envs/rstudio-tf-2.8/bin/python")
+reticulate::use_condaenv("rstudio-tf-2.8", conda="~/miniconda3/bin/conda", required = TRUE)
+
+  #Run Script: 
+library(tensorflow)
 tf_config() 
+tf_version() 
+tf$config$list_logical_devices()
+#If tf_config() crashes go back to Terminal and upgrade to python 3.9 conda install python=3.9
+
+  #Check again if all deps are listed conda list | grep tensorflow, for me tensorflow-macos and metal were missing after the python upgrade, I had to repeat step 8 and 9
+  #Restart 
+  #If numpy has issues - to install numpy: conda install -c anaconda numpy
+
 
 # Load simulated data
 
